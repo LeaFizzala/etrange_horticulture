e01d729f1407d3bce4503f0e686db92f53c1124b
Léa Fizzala <leafizzala.dev@gmail.com>
2022-11-17 17:58:00 +0100

    Manager and Controller for the User entities

---
+++ src/managers/UserManager.php
@@ -0,0 +1,20 @@
+<?php
+use Lib\Manager\AbstractManager;
+use App\entities\User;
+
+class UserManager extends AbstractManager
+{
+    public function find(int $id) {
+        return $this->findOne(User::class, [ 'id' => $id ]);
+    }
+
+    public function add(User $user) {
+        return $this->create(User::class, [
+                'prenom' => $user->getPrenom(),
+                'nom' => $user->getNom(),
+                'email' => $user->getEmail(),
+                'password' => $user->getPassword()
+            ]
+        );
+    }
+}
