ed488a92b6f2cd453d82ccce4da244242837cfd0
Léa Fizzala <leafizzala.dev@gmail.com>
2022-11-17 13:55:18 +0100

    Created const table for db info

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+const DB_INFOS = [
+    'host'     => 'localhost',
+    'port'     => '3306',
+    'dbname'   => 'strange_horticulture',
+    'username' => 'root',
+    'password' => ''
+];
+
+
+
+
