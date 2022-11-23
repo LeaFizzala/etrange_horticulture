73814d2db44b85f08b2995a8a2f2f0829c8bf5f3
Léa Fizzala <leafizzala.dev@gmail.com>
2022-11-23 16:46:08 +0100

    Only one question displayed at the beginning of the quizz

--- templates/quizz.php
+++ templates/quizz.php
@@ -1,19 +1,41 @@
 
 <section>
-<form id="plant-quizz" method="post" action="take-quizz.php">
-    <div class="form-group">
-        <?php if (isset($data['questions']))
-            foreach ($data['questions'] as $oneQuestion){
-                echo('<div class="container">');
-                echo($oneQuestion->getQuestion());
-                foreach ($oneQuestion->createAnswerList() as $answer){
-                    echo( '<input class="form-check-input" type="radio" name="question-5-answers" value=$number />
+    <form id="plant-quizz" method="post" action="take-quizz.php">
+        <div class="question-container" class="container" >
+            <?php if (isset($data['questions']))
+                foreach ($data['questions'] as $oneQuestion){
+
+                    //on crée une condition pour commencer par n'afficher que la première question
+                    if($oneQuestion == $data['questions'][0] ){
+                        echo('<div class="container">');
+                        echo('<div class="card shadow-sm" style="width: 30rem;"  >');
+                        echo('<div class="card-body">');
+                        echo('<h5 class="card-title">' . $oneQuestion->getQuestion() . '</h5>');
+                        foreach ($oneQuestion->createAnswerList() as $answer){
+                            echo( '<input class="form-check-input" type="radio" name=' . $oneQuestion->getQuestionNumber() . '   value=$number />
 <label class="form-check-label" for="inlineCheckbox2">' . $answer . '</label>' );
+                        }
+
+                        echo('</div>');
+                        echo('</div>');
+                        echo('</div>');
+                    }
+                    else{
+                        echo('<div class="invisible-question" class="container">');
+                        echo('<div class="card shadow-sm" style="width: 30rem;"  >');
+                        echo('<div class="card-body">');
+                        echo('<h5 class="card-title">' . $oneQuestion->getQuestion() . '</h5>');
+                        foreach ($oneQuestion->createAnswerList() as $answer){
+                            echo( '<input class="form-check-input" type="radio" name=' . $oneQuestion->getQuestionNumber() . '   value=$number />
+<label class="form-check-label" for="inlineCheckbox2">' . $answer . '</label>' );
+                        }
+
+                        echo('</div>');
+                        echo('</div>');
+                        echo('</div>');
+                    }
                 }
-                echo('<input type="submit" value=' . $oneQuestion->getButton() . '></input>');
-                echo('</div>');
-            }
-             ?>
-    </div>
-</form>
+            ?>
+        </div>
+    </form>
 </section>
